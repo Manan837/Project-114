1038f4ccb77ff7051f1d09f2e014eaf2a239c950
Manan837 <89606852+Manan837@users.noreply.github.com>
2023-03-17 18:42:30 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,28 @@ function setup()
 {
     canvas = createCanvas(300, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPose);
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet is initialized");
+}
+
+function gotPose(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+
+    }
 }
 
 function take_snapshot()
@@ -15,4 +37,5 @@ function take_snapshot()
 
 function draw()
 {
+    image(video, 0, 0, 300, 300);
 }
